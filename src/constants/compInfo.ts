3b3068583401affd1fe8c98521e4579f3c8e4c19
kanghyun98 <lkh981107@gmail.com>
2022-07-27 23:17:51 +0900

    chore: add company info to constants

--- src/constants/compInfo.ts
+++ src/constants/compInfo.ts
@@ -1,1 +1,15 @@
-export const PHONE_NUMBER = '010-XXXX-XXXX';
+export const COMP_INFO = {
+  number: {
+    tel_KR: '070-XXXX-XXXX',
+    tel_US: '070-XXXX-XXXX',
+    fax_KR: '070-XXXX-XXXX',
+    fax_US: '070-XXXX-XXXX',
+  },
+  address: {
+    office_KR: '경기도 김포시 김포대로 831번지 4층 412호',
+    office_US: '1412 Oakbrook DR. Norcross, GA 30093 USA',
+  },
+  email: {
+    main: 'sales@euromericar.com',
+  },
+};
