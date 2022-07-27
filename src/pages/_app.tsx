a0860a272e4524d4ec538d2c19726c95934b7cc0
kanghyun98 <lkh981107@gmail.com>
2022-07-27 15:11:34 +0900

    chore: add basic settings

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '@constants/styles/global.css';
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
