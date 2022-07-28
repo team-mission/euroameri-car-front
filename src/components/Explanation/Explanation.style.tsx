3890c9270843d4db2cbffe1721b5895cf07c6c95
kanghyun98 <lkh981107@gmail.com>
2022-07-28 14:26:26 +0900

    feat: add Explanation component

---
+++ src/components/Explanation/Explanation.style.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const ExplainWrapper = styled.section`
+  max-width: 1200px;
+  margin: 30px 70px;
+`;
+
+export const Text = styled.p`
+  width: 100%;
+  font-size: 1rem;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import MainHeader from '@components/MainHeader';
+import Explanation from '@components/Explanation';
 import Footer from '@components/Footer';
 
 const Home: NextPage = () => (
   <>
     <MainHeader />
+    <Explanation />
     <Footer />
   </>
 );
