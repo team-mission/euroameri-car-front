80d22968ece3b54e7d174df571d24d9e45d2b633
kanghyun98 <lkh981107@gmail.com>
2022-07-28 13:56:09 +0900

    feat: add Footer component incomplete mode

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -1,5 +1,27 @@
 import styled from '@emotion/styled';
 
-export const StyledFooter = styled.footer`
-  background-color: red;
+// TODO: 디자인 수정
+export const FooterWrapper = styled.footer`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 150px;
+  padding: 0 3rem;
+`;
+
+export const FlagWrapper = styled.img`
+  width: 100px;
+  height: 40px;
+`;
+
+export const CompInfoWrapper = styled.div`
+  height: 100%;
+  padding: 5px 0;
+`;
+
+export const Text = styled.p`
+  font-size: 0.75rem;
+  font-weight: 400;
+  margin: 0;
 `;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,21 @@
-import { PHONE_NUMBER } from '@constants/compInfo';
-import { StyledFooter } from './Footer.style';
+import { COMP_INFO } from '@constants/compInfo';
+import * as styles from './Footer.style';
 
-const Footer = () => <StyledFooter>{PHONE_NUMBER}</StyledFooter>;
+const Footer = () => (
+  <styles.FooterWrapper>
+    <styles.CompInfoWrapper>
+      <styles.FlagWrapper src="/images/FlagKR.png" />
+      <styles.Text>{COMP_INFO.address.office_KR}</styles.Text>
+      <styles.Text>{`Tel: ${COMP_INFO.number.tel_KR}`}</styles.Text>
+      <styles.Text>{`Fax: ${COMP_INFO.number.fax_KR}`}</styles.Text>
+    </styles.CompInfoWrapper>
+    <styles.CompInfoWrapper>
+      <styles.FlagWrapper src="/images/FlagUS.png" />
+      <styles.Text>{COMP_INFO.address.office_US}</styles.Text>
+      <styles.Text>{`Tel: ${COMP_INFO.number.tel_US}`}</styles.Text>
+      <styles.Text>{`Fax: ${COMP_INFO.number.fax_US}`}</styles.Text>
+    </styles.CompInfoWrapper>
+  </styles.FooterWrapper>
+);
 
 export default Footer;
