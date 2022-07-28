92e14c959b7520752549d36daf534f99b3b6c42c
kanghyun98 <lkh981107@gmail.com>
2022-07-28 18:26:49 +0900

    feat: add components to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,18 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 import MainHeader from '@components/MainHeader';
+import MainWrapper from '@components/MainWrapper';
 import Explanation from '@components/Explanation';
+import WareHouse from '@components/WareHouse';
 import Footer from '@components/Footer';
 
 const Home: NextPage = () => (
   <>
     <MainHeader />
-    <Explanation />
+    <MainWrapper>
+      <Explanation />
+      <WareHouse />
+    </MainWrapper>
     <Footer />
   </>
 );
