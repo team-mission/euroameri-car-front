6c8ffa6247bca035102befcb863d7e6b04cdcb98
kanghyun98 <lkh981107@gmail.com>
2022-07-31 00:20:44 +0900

    fix: make MainWrapper component style to center alignment

--- src/components/MainWrapper/MainWrapper.style.tsx
+++ src/components/MainWrapper/MainWrapper.style.tsx
@@ -3,5 +3,6 @@ import styled from '@emotion/styled';
 export const StyledMain = styled.main`
   position: relative;
   max-width: 1200px;
-  margin: 50px 70px;
+  padding: 50px 70px;
+  margin: auto;
 `;
