4642d395bddbd7b7bfba6be4c195e2c90d4deeeb
kanghyun98 <lkh981107@gmail.com>
2022-07-31 19:07:51 +0900

    feat: add constants for brands

---
+++ src/constants/brand.ts
@@ -0,0 +1,39 @@
+export const BRAND_TYPE = {
+  supercar: 'Supercar Auto Parts',
+  european: 'European Auto Parts',
+  us: 'The U.S. Auto Parts',
+};
+
+export const BRAND_NAME = {
+  supercar: [
+    'Aston_Martin',
+    'Bentley',
+    'Ferrari',
+    'Lambo',
+    'McLaren',
+    'Maserati',
+    'Rolls_Royce',
+  ],
+  european: [
+    'Alfa_Romeo',
+    'Audi',
+    'BMW',
+    'Jaguar',
+    'Land_Rover',
+    'Lotus',
+    'Mercedes',
+    'Porsche',
+    'VW',
+    'Volvo',
+  ],
+  us: [
+    'Cadillac',
+    'Chevrolet',
+    'Chrysler',
+    'Dodge',
+    'Ford',
+    'Hummer',
+    'Jeep',
+    'Lincoln',
+  ],
+};
