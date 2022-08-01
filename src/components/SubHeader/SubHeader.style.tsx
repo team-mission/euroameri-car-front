214c13bdd076bd88c614ec0b5d805bb6d9c864b0
kanghyun98 <lkh981107@gmail.com>
2022-08-01 21:36:28 +0900

    feat: add SubHeader component

---
+++ src/components/SubHeader/SubHeader.style.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+export const SubHeaderWrapper = styled.header`
+  position: relative;
+  width: 100%;
+  height: 67px;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+`;
+
+export const SubHeaderContent = styled.div`
+  position: relative;
+  display: flex;
+  max-width: 1200px;
+  height: 100%;
+  margin: auto;
+`;
+
+export const LogoWrapper = styled.div`
+  height: 100%;
+  width: 30%;
+`;
+
+export const LogoImg = styled.img`
+  height: 100%;
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 25px;
+  font-size: 1.25rem;
+  margin: 0;
+`;
