89daf1bd8937c008fea239172988617268356bd3
kanghyun98 <lkh981107@gmail.com>
2022-08-03 14:16:14 +0900

    feat: add benchmark-part-page

---
+++ src/constants/benchmark.ts
@@ -0,0 +1,27 @@
+export const BENCHMARK_BRAND = [
+  'Hyundai_Transys',
+  'Incheon_Airport',
+  'Sejong',
+  'Autoliv',
+  'Seoyon_Ehwa',
+  'Seoyon_Intech',
+  'Infac',
+  'Tenergy',
+  'Kamtec',
+  'Jmft',
+  'Faurecia',
+] as const;
+
+export const BENCHMARK_INFO = {
+  Hyundai_Transys: ['powertrains', 'engine/trans', 'drive shafts', 'ev motors'],
+  Incheon_Airport: ['snow vehicle parts', 'bhs parts', 'various filters'],
+  Sejong: ['catalytic converters', 'center/rear mufflers'],
+  Autoliv: ['airbags', 'seat belts', 'dashboards/trims'],
+  Seoyon_Ehwa: ['auto seats', 'bus seats', 'door trims'],
+  Seoyon_Intech: ['auto seats', 'midium bus seats'],
+  Infac: ['coolant valves', 'actuators', 'turbo chargers'],
+  Tenergy: ['transfer cases', 'gear motors', 'sensors'],
+  Kamtec: ['turbo chargers', 'various valves'],
+  Jmft: ['differentials'],
+  Faurecia: ['the rods'],
+};
