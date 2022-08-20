340255b944fd06a419481c401a6325784305097d
kanghyun98 <lkh981107@gmail.com>
2022-08-20 23:51:51 +0900

    refactor: change firebase settings

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,29 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore/lite"
-import 'firebase/compat/firestore';
-import firebase from "firebase/compat/app";
+import { initializeApp } from 'firebase/app';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDx2XiYt3huY7JCYNIe_uEOWpC4za5hg3A",
-  authDomain: "euroameri-car.firebaseapp.com",
-  projectId: "euroameri-car",
-  storageBucket: "euroameri-car.appspot.com",
-  messagingSenderId: "519925930876",
-  appId: "1:519925930876:web:4b01bd96e6220b012422f9",
-  measurementId: "G-LFW4N8K2FE"
+// firebase configuration
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-const app = firebase.initializeApp(firebaseConfig);
-const firestore = firebase.firestore();
-const db = getFirestore(app);
+const firebaseApp = initializeApp(firebaseConfig);
 
-
-// Initialize Firebase
-export { db };
-
+export default firebaseApp;
