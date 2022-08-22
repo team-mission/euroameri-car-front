30d82be720599a93250d52454eeafbfca9ae2a6a
kanghyun98 <lkh981107@gmail.com>
2022-08-23 03:26:37 +0900

    refactor: change carousel logic

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,16 +1,31 @@
+import { useState, useCallback } from 'react';
+
+import { IMG_URL } from '@constants';
 import ThumbnailList from './ThumbnailList';
 import ImageDetail from './ImageDetail';
 
 interface CarouselProps {
-  imgUrl: string;
-  onClickThumbnail: (url: string) => void;
+  images: string[];
+  title: string;
 }
 
-const Carousel = ({ imgUrl, onClickThumbnail }: CarouselProps) => (
-  <>
-    <ThumbnailList onClickThumbnail={onClickThumbnail} />
-    <ImageDetail url={imgUrl} />
-  </>
-);
+const Carousel = ({ images, title }: CarouselProps) => {
+  const [imgUrl, setImgUrl] = useState(images[0]);
+
+  const onClickThumbnail = useCallback((url: string) => {
+    setImgUrl(url);
+  }, []);
+
+  return (
+    <>
+      <ThumbnailList
+        images={images}
+        title={title}
+        onClickThumbnail={onClickThumbnail}
+      />
+      <ImageDetail url={imgUrl} />
+    </>
+  );
+};
 
 export default Carousel;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { useState, useCallback } from 'react';
 
-import { IMG_URL } from '@constants/url';
+import { WAREHOUSE_IMG_COUNT, IMG_URL } from '@constants';
 import MainHeader from '@components/MainHeader';
 import MainWrapper from '@components/MainWrapper';
 import Explanation from '@components/Explanation';
@@ -10,18 +9,16 @@ import Carousel from '@components/Carousel';
 import Footer from '@components/Footer';
 
 const Home: NextPage = () => {
-  const [imgUrl, setImgUrl] = useState(`${IMG_URL.warehouse_basic}0.png`);
-
-  const onClickThumbnail = useCallback((url: string) => {
-    setImgUrl(url);
-  }, []);
+  const imgUrlArr = Array(WAREHOUSE_IMG_COUNT)
+    .fill(undefined)
+    .map((_, i) => `${IMG_URL.warehouse_basic}${i}.png`);
 
   return (
     <>
       <MainHeader />
       <MainWrapper>
         <Explanation />
-        <Carousel imgUrl={imgUrl} onClickThumbnail={onClickThumbnail} />
+        <Carousel images={imgUrlArr} title="창고 사진" />
       </MainWrapper>
       <Footer />
     </>
