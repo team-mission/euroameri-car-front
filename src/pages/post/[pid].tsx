cc2c4576198046caa5b6509502d9d72d7d772474
kanghyun98 <lkh981107@gmail.com>
2022-08-23 03:29:58 +0900

    feat: add PostDetail Page

--- src/pages/post/[pid].tsx
+++ src/pages/post/[pid].tsx
@@ -5,40 +5,43 @@ import { useState, useEffect } from 'react';
 import MainHeader from '@components/MainHeader';
 import SubHeader from '@components/SubHeader';
 import MainWrapper from '@components/MainWrapper';
-
+import PostContent from '@components/PostContent';
 import Footer from '@components/Footer';
-import GuideMent from '@components/GuideMent';
 
-import { getPostDetail } from '@service/database';
+import { getPostDetailAsync } from '@apis/post';
+import { PostDetailType } from '@apis/type';
 
 const PostDetailPage: NextPage = () => {
   const router = useRouter();
-  const { pid } = router.query;
+  const postId = Number(router.query.pid);
 
-  const [postData, setPostData] = useState({});
+  const [postData, setPostData] = useState<PostDetailType | {}>({});
+  const [password, setPassword] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    if (!router.isReady) {
+    if (!router.isReady || !postId) {
       return;
     }
 
     async function updateData() {
-      const postDetailData = await getPostDetail(pid);
+      const res = await getPostDetailAsync(postId, password);
 
-      if (postDetailData) {
-        setPostData(postDetailData);
+      if (res.isSuccess) {
+        setPostData(res.result);
       }
+
+      // TODO: 에러 처리 (비밀번호?)
     }
 
     updateData();
-  }, [pid, router.isReady]);
+  }, [password, postId, router.isReady]);
 
   return (
     <>
       <MainHeader />
       <SubHeader title="게시판" />
       <MainWrapper>
-        <h1>게시물 작성자: {postData.name}</h1>
+        <PostContent data={postData} />
       </MainWrapper>
       <Footer />
     </>
