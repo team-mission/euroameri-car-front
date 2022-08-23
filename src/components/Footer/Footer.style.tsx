c04c34effff068677eebb0cef2e57ddddf17f2c4
kanghyun98 <lkh981107@gmail.com>
2022-08-23 22:30:15 +0900

    feat: add admin-login page

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -24,6 +24,12 @@ export const CompInfoWrapper = styled.div`
   padding: 5px 0;
 `;
 
+export const Center = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const EmailArea = styled.div`
   display: flex;
   align-items: center;
@@ -35,4 +41,10 @@ export const Text = styled.p`
   font-size: 0.75rem;
   font-weight: 400;
   margin: 0;
+`;
+
+export const AdminButton = styled.div`
+  font-size: 0.8rem;
+  font-weight: 400;
+  cursor: pointer;
 `;
--- src/components/MainWrapper/MainWrapper.style.tsx
+++ src/components/MainWrapper/MainWrapper.style.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const StyledMain = styled.main`
   position: relative;
   max-width: 1200px;
+  min-height: 400px;
   padding: 50px;
   margin: auto;
 `;
