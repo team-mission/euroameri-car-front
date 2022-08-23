f996a91609078a401e796cf43b565a05477e7dcf
kanghyun98 <lkh981107@gmail.com>
2022-08-23 22:28:31 +0900

    feat: add admin button to Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,45 @@
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
+
 import { COMP_INFO, IMG_URL } from '@constants';
 import ImgWrapper from '@components/ImgWrapper';
 import * as styles from './Footer.style';
 
-const Footer = () => (
-  <styles.FooterWrapper>
-    <styles.CompInfoWrapper>
-      <styles.FlagImgWrapper>
-        <ImgWrapper src={IMG_URL.flag_KR} />
-      </styles.FlagImgWrapper>
-      <styles.Text>{COMP_INFO.address.office_KR}</styles.Text>
-      <styles.Text>{`Tel: ${COMP_INFO.number.tel_KR}`}</styles.Text>
-      <styles.Text>{`Fax: ${COMP_INFO.number.fax_KR}`}</styles.Text>
-    </styles.CompInfoWrapper>
-    <styles.EmailArea>
-      <styles.Text>{COMP_INFO.email.main}</styles.Text>
-    </styles.EmailArea>
-    <styles.CompInfoWrapper>
-      <styles.FlagImgWrapper>
-        <ImgWrapper src={IMG_URL.flag_US} />
-      </styles.FlagImgWrapper>
-      <styles.Text>{COMP_INFO.address.office_US}</styles.Text>
-      <styles.Text>{`Tel: ${COMP_INFO.number.tel_US}`}</styles.Text>
-      <styles.Text>{`Fax: ${COMP_INFO.number.fax_US}`}</styles.Text>
-    </styles.CompInfoWrapper>
-  </styles.FooterWrapper>
-);
+const Footer = () => {
+  const router = useRouter();
+
+  const moveAdminLoginPage = useCallback(() => {
+    router.push(`/admin`);
+  }, [router]);
+
+  return (
+    <styles.FooterWrapper>
+      <styles.CompInfoWrapper>
+        <styles.FlagImgWrapper>
+          <ImgWrapper src={IMG_URL.flag_KR} />
+        </styles.FlagImgWrapper>
+        <styles.Text>{COMP_INFO.address.office_KR}</styles.Text>
+        <styles.Text>{`Tel: ${COMP_INFO.number.tel_KR}`}</styles.Text>
+        <styles.Text>{`Fax: ${COMP_INFO.number.fax_KR}`}</styles.Text>
+      </styles.CompInfoWrapper>
+      <styles.Center>
+        <styles.EmailArea>
+          <styles.Text>{COMP_INFO.email.main}</styles.Text>
+        </styles.EmailArea>
+        <styles.AdminButton onClick={moveAdminLoginPage}>
+          ADMIN
+        </styles.AdminButton>
+      </styles.Center>
+      <styles.CompInfoWrapper>
+        <styles.FlagImgWrapper>
+          <ImgWrapper src={IMG_URL.flag_US} />
+        </styles.FlagImgWrapper>
+        <styles.Text>{COMP_INFO.address.office_US}</styles.Text>
+        <styles.Text>{`Tel: ${COMP_INFO.number.tel_US}`}</styles.Text>
+        <styles.Text>{`Fax: ${COMP_INFO.number.fax_US}`}</styles.Text>
+      </styles.CompInfoWrapper>
+    </styles.FooterWrapper>
+  );
+};
 
 export default Footer;
