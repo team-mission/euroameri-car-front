b6d93a2c8b53a6b011558fe35769b79909edad53
kanghyun98 <lkh981107@gmail.com>
2022-08-24 22:30:32 +0900

    feat: add post-write page

---
+++ src/pages/post/write.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next';
+import Script from 'next/script';
+
+import MainHeader from '@components/MainHeader';
+import SubHeader from '@components/SubHeader';
+import MainWrapper from '@components/MainWrapper';
+import PostEditor from '@components/PostEditor';
+import Footer from '@components/Footer';
+
+const PostWritePage: NextPage = () => (
+  <>
+    <Script
+      type="text/javascript"
+      src="https://www.google.com/recaptcha/api.js?render=6LfeB6QhAAAAAKqWkFbqT7ibvdB_9TsyCTcPypP-"
+    />
+    <MainHeader />
+    <SubHeader title="게시판" />
+    <MainWrapper>
+      <PostEditor />
+    </MainWrapper>
+    <Footer />
+  </>
+);
+
+export default PostWritePage;
