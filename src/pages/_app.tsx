05c4029cfaeb51bd1aa171246c9d43db7d13ae8b
kanghyun98 <lkh981107@gmail.com>
2022-08-27 01:42:04 +0900

    feat: add meta tags to head for seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,37 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@constants/styles/global.css';
 import 'antd/dist/antd.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>유로아메리카</title>
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <meta name="description" content="유럽 미국 일본 외산차 및 부품 유통" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+      <meta property="og:title" content="유로아메리카" />
+      <meta property="og:type" content="website" />
+      {/* <meta property="og:image" content="{img url}" /> */}
+      <meta
+        property="og:description"
+        content="유럽 미국 일본 외산차 및 부품 유통"
+      />
+      <meta property="og:site_name" content="유로아메리카" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="유로아메리카" />
+      <meta
+        name="twitter:description"
+        content="유럽 미국 일본 외산차 및 부품 유통"
+      />
+      {/* <meta name="twitter:image" content="{img url}" /> */}
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
