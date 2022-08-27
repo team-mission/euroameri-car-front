e7820c16857c52d152e05337d3fd8913c08b9f11
kanghyun98 <lkh981107@gmail.com>
2022-08-27 23:44:19 +0900

    add code for seo and optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,46 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@constants/styles/global.css';
 import 'antd/dist/antd.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      <meta charSet="utf-8" />
+      <title>유로아메리카</title>
+      <link rel="icon" href="/images/favicon.ico" />
+      <meta name="description" content="유럽 미국 일본 외산차 및 부품 유통" />
+      <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+      <meta property="og:title" content="유로아메리카" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/images/sns.png" />
+      <meta
+        property="og:description"
+        content="유럽 미국 일본 외산차 및 부품 유통"
+      />
+      <meta property="og:site_name" content="유로아메리카" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="유로아메리카" />
+      <meta
+        name="twitter:description"
+        content="유럽 미국 일본 외산차 및 부품 유통"
+      />
+      <meta name="twitter:image" content="/images/sns.png" />
+
+      <meta
+        name="naver-site-verification"
+        content="cd3ec593ae93038fdf99c2ee6b2a2e2c35735a90"
+      />
+      <meta
+        name="google-site-verification"
+        content="nLVdsdqM3BmqoHt3EO-E9YimaXTlbZqtHVM5R4k4V3g"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default MyApp;
